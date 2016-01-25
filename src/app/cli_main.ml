99e092f74fea12674235e2d11aa2fc8ef465a809
Sebastien Mondet <seb@mondet.org>
2016-01-25 16:09:08 -0500

    Add versioning to the JSON format

--- src/app/cli_main.ml
+++ src/app/cli_main.ml
@@ -2,8 +2,8 @@
 let () =
   Redaw.Dataset.(
     let dummy = paired_end ~r1:[] ~r2:[] in
-    static ~host:Local ~name:"World"
-      (Dna { normal = dummy; tumors = []})
+    static  ~name:"World"
+      (dna ~normal:dummy ~tumors:[])
       None
       |> name
   ) |> Printf.printf "Hello %s\n%!"
--- src/lib/dataset.ml
+++ src/lib/dataset.ml
@@ -1,47 +1,48 @@
-open Nonstd
+open Internal_pervasives
 
-module String = Sosa.Native_string
-let ( // ) = Filename.concat
+module V0 = struct
+  type path = string
+    [@@deriving yojson]
 
-let failwithf fmt = ksprintf failwith fmt
+  type 'a sample =
+    | Paired_end of 'a list * 'a list
+    [@@deriving yojson]
 
+  type 'a somatic =
+    {normal : 'a sample; tumors : 'a sample list}
+    [@@deriving yojson]
 
-type path = string
-[@@deriving yojson]
+  type 'a rna =
+      Rna of 'a sample
+    [@@deriving yojson]
 
-type 'a sample =
-  | Paired_end of 'a list * 'a list
-[@@deriving yojson]
+  type 'a dna =
+      Dna of 'a somatic
+    [@@deriving yojson]
 
-type 'a somatic =
-  {normal : 'a sample; tumors : 'a sample list}
-[@@deriving yojson]
+  type host =
+    | Uri of string
+    | Local
+    [@@deriving yojson]
 
-type 'a rna =
-  Rna of 'a sample
-[@@deriving yojson]
+  type location =
+    | Static of host * path dna * path rna option
+    [@@deriving yojson]
 
-type 'a dna =
-  Dna of 'a somatic
-[@@deriving yojson]
+  type t = {
+    name: string;
+    location: location;
+  }
+    [@@deriving yojson]
+end
 
-type host =
-  | Uri of string
-  | Local
-[@@deriving yojson]
+open V0
 
-type location =
-  | Static of host * path dna * path rna option
-[@@deriving yojson]
-
-type t = {
-  name: string;
-  location: location;
-}
-[@@deriving yojson]
-
+let dna ~normal ~tumors = Dna {normal; tumors}
 let paired_end ~r1 ~r2 = Paired_end (r1, r2)
-let static ~host ~name dna_sample rna_sample =
+let static ?(host = Local) ~name dna_sample rna_sample =
   {name; location = Static (host, dna_sample, rna_sample)}
 
 let name t = t.name
+
+include Json.Versioned.Of_v0(V0)
